4573c3403b327e89c06ab2644d91910a14c012b7
Juan Camilo Mora <jcmora@smdigital.com.co>
2018-01-03 15:09:37 -0500

    Initial commit

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void goToCredits() {
+		SceneManager.LoadScene ("About");
+	}
+
+	public void goToIntro() {
+		SceneManager.LoadScene ("Intro");
+	}
+
+	public void goToGame() {
+		SceneManager.LoadScene ("Game");
+	}
+
+}
