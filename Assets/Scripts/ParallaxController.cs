6869e4ab2b1af6e20e29bb22ec39dfe18c7706ca
Juan Camilo Mora <jcmora@smdigital.com.co>
2018-01-15 21:13:15 -0500

    Se programa el background parallax de la montaña intermedia

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxController : MonoBehaviour {
+
+	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+	}
+}
