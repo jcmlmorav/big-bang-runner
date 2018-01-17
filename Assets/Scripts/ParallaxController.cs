8187e82812b282d28988e17c00d0f775bfd326e5
mmarin <>
2018-01-16 20:22:42 -0500

    Se ajusta la forma de generar el suelo

--- Assets/Scripts/ParallaxController.cs
+++ Assets/Scripts/ParallaxController.cs
@@ -7,6 +7,7 @@ public class ParallaxController : MonoBehaviour {
 
 	public float parallaxSpeed = 0.01f;
 	public RawImage background;
+	public RawImage platform;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +18,6 @@ public class ParallaxController : MonoBehaviour {
 	void Update () {
 		float finalSpeed = parallaxSpeed * Time.deltaTime;
 		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+		platform.uvRect = new Rect (platform.uvRect.x + finalSpeed * 8, 0f, 1f, 1f);
 	}
 }
--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour {
 
-    public float speed = 2f;
+    public float speed = 2.5f;
 
 	void Start () {
         
