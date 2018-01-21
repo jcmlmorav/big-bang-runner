a4bd877bcd9dafc74932f426a59fab626dfbce86
Juan Camilo Mora <jcmora@smdigital.com.co>
2018-01-21 11:01:30 -0500

    Se organiza la estructura de carpetas, se optimiza la escena de la intro y se crea el controlador general del juego

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public void LoadGame() {
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void LoadAbout(){
+		SceneManager.LoadScene ("About");
+	}
+
+	public void Exit() {
+		// Pendiente programar la cerrada del juego
+	}
+
+
+}
